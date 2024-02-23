16c1a5ddc1966a47f2f57cc851c207407c00c56d
browda345 <darren.bronw134@protonmail.com>
2024-02-23 14:55:37 +1100

    update to vite

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,7 +1,6 @@
 import { Fragment  } from 'react';
 import { Container} from 'semantic-ui-react';
 import NavBar from './NavBar';
-
 import { observer } from 'mobx-react-lite';
 import { Outlet, useLocation } from 'react-router-dom';
 import Homepage from '../../features/home/Homepage';
